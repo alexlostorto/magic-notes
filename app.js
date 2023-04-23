51911a9c18db9f3e044f831b7bf6e4e86280793c
Alex Lo Storto <lostorto.business@gmail.com>
2023-04-23 12:21:31 +0100

    Add copy and download functionality back to website

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ credits();
 const headerText = document.querySelector('.header-text');
 const headerDescription = document.querySelector('.header-description');
 const headerArrow = document.querySelector('.arrow');
+const copyButton = document.querySelector('#copy-button');
 
 fadeIn(headerText, 1000);
 fadeIn(headerDescription, 1500);
 fadeIn(headerArrow, 2000);
+
+const script = `s=document.createElement('script');s.src='https://cdn.jsdelivr.net/gh/alexlostorto/magic-notes@v3.0.0/console/inject.js';document.head.appendChild(s);`;
+
+copyButton.addEventListener('click', () => {
+    copy(script, copyButton, message='Copied!', initialText='Copy', errorMessage='Failed to copy', delay=1000);
+});
--- install/app.js
+++ install/app.js
@@ -4,7 +4,24 @@ credits();
 const headerText = document.querySelector('.header-text');
 const headerDescription = document.querySelector('.header-description');
 const instructions = document.querySelector('.slide article');
+const downloadButton = document.querySelector('#download-button');
 
 fadeIn(headerText, 800);
 fadeIn(headerDescription, 1000);
 fadeIn(instructions, 1300);
+
+function download(uri, name) {
+    var link = document.createElement("a");
+    link.setAttribute('download', name);
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click(); 
+    link.remove();
+}
+
+downloadButton.addEventListener('click', async () => {
+    download('documents/magic-notes.zip', 'Magic Notes');
+    downloadButton.textContent = 'Downloaded!';
+    await sleep(1000);
+    downloadButton.textContent = 'Download ZIP';
+})
